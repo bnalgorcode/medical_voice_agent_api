f0c13b0f72c763df0b6875771300284cfa8b91a9
bnalgorcode <209712411+bnalgorcode@users.noreply.github.com>
2025-09-17 23:38:43 -0700

    added -- A quick routes debugger

--- src/app.js
+++ src/app.js
@@ -25,4 +25,17 @@ app.use("/api/doctors", doctorRoutes);
 app.use("/api/zoho", zohoRoutes);
 app.use("/api/patients", patientRoutes);
 
+app.get("/_routes", (req, res) => {
+  const stack = app._router?.stack || [];
+  const routes = [];
+  stack.forEach((m) => {
+    if (m.route && m.route.path) {
+      const methods = Object.keys(m.route.methods).join(",").toUpperCase();
+      routes.push(`${methods} ${m.route.path}`);
+    }
+  });
+  res.json({ routes });
+});
+
+
 module.exports = app;
